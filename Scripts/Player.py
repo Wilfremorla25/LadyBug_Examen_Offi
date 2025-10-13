6e2850e5a57372229973b7487c9d48a5b9571c5a
Wilfredemorla25 <wilfredemorla4321@gmail.com>
2025-10-13 15:11:57 -0400

    POSICION DE FILAS Y COLUMNA< LIMITE DEL MAPA, VERIFICAR COLISIONES

--- Scripts/Player.py
+++ Scripts/Player.py
@@ -12,3 +12,23 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface([TILE_SIZE, TILE_SIZE])
         self.image.fill((0, 255, 0))
         self.rect = self.image.get_rect()
+        
+        #POSICION DE FILAS Y COLUMNAS
+        self.row = start_row
+        self.col = start_col
+        self._update_pixels_position()
+    def _update_pixels_position(self):
+        self.rect.x = self.col * TILE_SIZE
+        self.rect.y = self.row * TILE_SIZE
+        
+    def move(self, dx, dy, maze_map):
+        new_col = self.col + dx
+        new_row = self.row + dy
+        
+        #LIMITE DEL MAPA
+        if not (0 <= new_row < len(maze_map) and 0 <= new_col < len(maze_map[0])):
+            return
+        target_tile = maze_map[new_row] [new_col]
+        #VERIFICAR COLISIONES
+        if target_tile == TILE_WALL:
+            return
